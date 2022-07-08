55279cb934debe16872d7c2ebb38b771ee4f3bde
Padma Venkatraman <vpadma@berkeley.edu>
2022-07-08 10:29:18 -0700

    Minor fixes (added date, lat, lon) to BITtoHdf5

--- src/mangonetwork/raw/BITtoHDF5.py
+++ src/mangonetwork/raw/BITtoHDF5.py
@@ -1,19 +1,29 @@
+import sys
+
 import numpy as np
 import PIL
 import os
 import h5py
 import argparse
+from datetime import datetime
+
+if sys.version_info < (3,9):
+    import importlib_resources as resources
+else:
+    from importlib import resources
+
+import pandas as pd
 
 class BITtoHDF5:
 
-    def __init__(self, cf, bitfile, hdf5file):
+    def __init__(self, date, cf, bitfile, hdf5file):
         # inputs are config file, bitfile you want to convert, name of output hdf5 file
+        self.date = date
         self.configFile = cf
         self.inputFile = bitfile
         self.outputFile = hdf5file
-        #self.letter_to_site = {'C': 'Capitol Reef Field Station', 'B': 'Bridger', 'H'}
-        self.letter_to_code = {'C': 'cfs', 'm': 'mad', 'h': 'hco'}
 
+        # extract site, time
 
     def convert_to_PNG(self):
         f = open(self.inputFile, "rb")
@@ -32,14 +42,22 @@ class BITtoHDF5:
     # the old sites are bridger, cfs, hat creek observatory, madison - is bridger brg?
     def write_to_HDF5(self):
 
-        self.metadata = dict(
-            start_time=self.inputFile[1:-4],
-            station=self.letter_to_code[self.inputFile[0]],
+        time_str = self.inputFile[1:-4]
+        try:
+            start_obj = datetime.strptime(self.date + time_str, '%Y%m%d%H%M%S')
+        except ValueError:
+            start_obj = datetime.strptime(self.date + time_str, '%b%d%Y%H%M%S')
 
-            #maybe store lon and lat in config file? it's already there in the csv though.
-            # TODO
-            latitude="",
-            longitude="",
+        site_info = pd.read_csv(resources.files('mangonetwork.raw.data').joinpath('SiteInformation.csv'))
+        site_code_letter = self.inputFile[0]
+        row = site_info[site_info['Site Code']==site_code_letter]
+
+        self.metadata = dict(
+            # I think the time is already in UTC - check whether I have to make any conversions.
+            start_time=start_obj.timestamp(),
+            station=row['Site Abbreviation'].item(),
+            latitude=row['Center Latitude'].item(),
+            longitude=row['Center Longitude'].item(),
 
             serialnum="",
             device_name='Atik 414ex', # confirm
@@ -57,8 +75,9 @@ class BITtoHDF5:
             bin_y="",
             ccd_temp="",
             set_point="",
-            image_bytes="",
+            image_bytes=""
         )
+
         UnitsCatalog = dict(
             version='',
             start_time='Unix timetamp (UTC)',
@@ -106,6 +125,9 @@ def parse_args():
 
     parser = argparse.ArgumentParser(description='Accepting config, input BIT file and output HDF5 filename'
                                                  'to convert BIT to HDF5.')
+
+    parser.add_argument('-d', '--date', dest = 'date', type=str,
+                        help = 'Date in MMDDYYYY format.', required = True)
     parser.add_argument('-c', '--config', dest='config', type=str,
                         help='Config file containing ___.', required = True)
     parser.add_argument('-if', '--inputFile', dest='inputFile', nargs=1, type=str,
@@ -121,6 +143,7 @@ def parse_args():
 
 def main():
     command_line_args = parse_args()
+    date = command_line_args.date
     conf = command_line_args.config
     output = command_line_args.output
     #what should the output be? I think this changes everything
@@ -128,7 +151,7 @@ def main():
 
     # i've added a list component but idk how we would customize the output names
     # in that case, we might have to remove that argument
-    BITtoHDF5(conf, inputFile, output)
+    BITtoHDF5(date, conf, inputFile, output)
 
 if __name__ == '__main__':
     main()
