Fix rotation and inversion issues throughout pipeline.

src/mangonetwork/raw/Calibrate.py
@@ -5,58 +5,85 @@ import h5py
 import os
 import argparse
 import configparser
+import logging
+import io
+import sys
+if sys.version_info < (3,9):
+    import importlib_resources as resources
+else:
+    from importlib import resources
 
 
 class Calibrate:
 
-    def __init__(self, configFile):
+    def __init__(self, configFile, starCalFile):
 
-        self.calParams = {}
+        # self.calParams = {}
 
-        self.read_config(configFile)
+        # config = self.read_config(configFile)
 
-        self.star_data = np.loadtxt(self.starCalFile, usecols=(1,2,3,4))
+        # self.starCalFile = starCalFile
+
+        # Make function for this
+        # Add header infomation to star cal files
+        # f_handler = io.StringIO(starCalFile)
+        self.star_az, self.star_el, self.x, self.y = np.loadtxt(io.StringIO(starCalFile), usecols=(1,2,3,4), unpack=True)
+        # self.x = 695.-self.x
 
         self.RE = 6371.0
 
         self.find_calibration_params()
-        self.create_calibration_arrays()
-        self.save_calibration_file()
+        self.save_calibration_params(configFile)
+        # self.create_calibration_arrays()
+        # self.save_calibration_file()
 
 
-    def read_config(self, configFile):
-        # read in config file
-        config = configparser.ConfigParser()
-        config.read(configFile)
+    # def read_config(self, configFile):
+    #     # read in config file
+    #     # print(configFile)
+    #     config = configparser.ConfigParser()
+    #     config.read(configFile)
+    #     # for k in config.keys():
+    #     #     print(k)
+    #
+    #     # self.siteCalFilepath = config['MOCK']['SITE_CAL_FILEPATH']
+    #     # site calibration folders are all located in:
+    #     # ~/Sites/<site name>/calibration
+    #     # self.rotationAngle = float(config['MOCK']['ROTATION_ANGLE'])
+    #     # self.outputFile = config.get('CALIBRATION','CALIBRATION_FILE')
+    #     # self.starCalFile = config.get('CALIBRATION', 'STAR_CAL_FILE')
+    #     # self.ha = config.getfloat('CALIBRATION', 'ALTITUDE')
+    #     # self.elevCutoff = config.getfloat('CALIBRATION', 'ELEV_CUTOFF')
+    #     # self.Imax = config.getint('CALIBRATION','IMAX')
+    #     # self.Jmax = config.getint('CALIBRATION','JMAX')
+    #     # self.newImax = config.getint('CALIBRATION','NEWIMAX')
+    #     # self.newJmax = config.getint('CALIBRATION','NEWJMAX')
+    #     # self.siteLat = config.getfloat('CALIBRATION','SITELAT')
+    #     # self.siteLon = config.getfloat('CALIBRATION','SITELON')
+    #     return config
 
-        # self.siteCalFilepath = config['MOCK']['SITE_CAL_FILEPATH']
-        # site calibration folders are all located in:
-        # ~/Sites/<site name>/calibration
-        # self.rotationAngle = float(config['MOCK']['ROTATION_ANGLE'])
-        self.outputFile = config.get('CALIBRATION','CALIBRATION_FILE')
-        self.starCalFile = config.get('CALIBRATION', 'STAR_CAL_FILE')
-        self.ha = config.getfloat('CALIBRATION', 'ALTITUDE')
-        self.elevCutoff = config.getfloat('CALIBRATION', 'ELEV_CUTOFF')
-        self.Imax = config.getint('CALIBRATION','IMAX')
-        self.Jmax = config.getint('CALIBRATION','JMAX')
-        # self.newImax = config.getint('CALIBRATION','NEWIMAX')
-        # self.newJmax = config.getint('CALIBRATION','NEWJMAX')
-        # self.siteLat = config.getfloat('CALIBRATION','SITELAT')
-        # self.siteLon = config.getfloat('CALIBRATION','SITELON')
-
-    def normalize_pixel_coords(self, i, j):
-        RL = self.Jmax/2.
-        x = (self.Imax/2.-i)/RL
-        y = (self.Jmax/2.-j)/RL
-        return x, y
+    # def normalize_pixel_coords(self, i, j):
+    #     # Normalization is a problem
+    #     # Ideally want to fit to normalization factor???
+    #     # Find RL at elev=0
+    #     RL = self.Jmax/2.
+    #     x = (self.Imax/2.-i)/RL
+    #     y = (self.Jmax/2.-j)/RL
+    #     return x, y
 
     def find_calibration_params(self):
 
-        star_el = self.star_data[:,0]*np.pi/180.
-        star_az = self.star_data[:,1]*np.pi/180.
-        i = self.star_data[:,2]
-        j = self.star_data[:,3]
-        x, y = self.normalize_pixel_coords(i, j)
+        # star_el = self.star_data[:,0]*np.pi/180.
+        # star_az = self.star_data[:,1]*np.pi/180.
+        # # x = 695-self.star_data[:,2] # Flip stars left-right same as image needs to be flipped left-right
+        # x = self.star_data[:,2] # Flip stars left-right same as image needs to be flipped left-right
+        # y = self.star_data[:,3]
+
+        star_az = self.star_az*np.pi/180.
+        star_el = self.star_el*np.pi/180.
+        x = self.x
+        y = self.y
+        # x, y = self.normalize_pixel_coords(i, j)
         xp = np.cos(star_el)*np.sin(star_az)
         yp = np.cos(star_el)*np.cos(star_az)
 
@@ -65,16 +92,36 @@ class Calibrate:
 
         init_params = self.initial_params(x, y, xp, yp)
         params = least_squares(self.residuals, init_params, args=(x, y, xp, yp))
-        self.x0, self.y0, self.theta, self.A, self.B, self.C, self.D = params.x
+        self.x0, self.y0, self.rl, self.theta, self.C, self.D = params.x
+        self.A = np.pi/2.
+        self.B = -(np.pi/2. + self.C + self.D)
 
+        xt, yt = self.transform(x, y, self.x0, self.y0, self.rl, self.theta, self.A, self.B, self.C, self.D, unwarp=False)
+        # xt, yt = self.transform(x, y, 695./2., 519./2., 519./2., 15., self.A, self.B, self.C, self.D, unwarp=False)
+        import matplotlib.pyplot as plt
+        plt.scatter(xp, yp)
+        plt.scatter(xt, yt)
+        # r = np.linspace(0., 1., 100)
+        # lam = self.A + self.B*r + self.C*r**2 + self.D*r**3
+        # plt.plot(r, lam*180./np.pi)
+        # x1 = (x - self.x0)/self.rl
+        # y1 = (y - self.y0)/self.rl
+        # r1 = np.sqrt(x1**2+y1**2)
+        # plt.scatter(r1, star_el*180./np.pi)
+        # plt.grid(which='both', axis='both')
+        plt.show()
 
-    def transform(self, x, y, x0, y0, theta, A, B, C, D, unwarp=True, return_elev=False):
-        x1 = x - x0
-        y1 = y - y0
+    # share funciton between processing and calibration classes?
+    # shared utilities script?
+    def transform(self, x, y, x0, y0, rl, theta, A, B, C, D, unwarp=True, return_elev=False):
+        x1 = (x - x0)/rl
+        y1 = (y - y0)/rl
 
         t = theta*np.pi/180.
         x2 = np.cos(t)*x1 - np.sin(t)*y1
         y2 = np.sin(t)*x1 + np.cos(t)*y1
+        # x2 = np.cos(t)*x1 + np.sin(t)*y1
+        # y2 = -np.sin(t)*x1 + np.cos(t)*y1
 
         r = np.sqrt(x2**2+y2**2)
         lam = A + B*r + C*r**2 + D*r**3
@@ -96,8 +143,11 @@ class Calibrate:
             return x3, y3
 
     def residuals(self, params, x, y, xp, yp):
-        x0, y0, theta, A, B, C, D = params
-        xt, yt = self.transform(x, y, x0, y0, theta, A, B, C, D, unwarp=False)
+        x0, y0, rl, theta, C, D = params
+        A = np.pi/2.
+        B = -(np.pi/2. + C + D)
+        xt, yt = self.transform(x, y, x0, y0, rl, theta, A, B, C, D, unwarp=False)
+        # print(xt, xp)
         res = np.sqrt((xp-xt)**2 + (yp-yt)**2)
         return res
 
@@ -106,11 +156,12 @@ class Calibrate:
 
         # Get these from config file?
         # TBH, unless a camera is oriented REALLY wierdly, these initial parameters should be fine
-        x0, y0 = [0., 0.]
+        # x0, y0 = [0., 0.]
+        x0, y0, rl = [347.5, 259.5, 259.5]
         A, B, C, D = [np.pi/2, -np.pi/2, 0., 0.]
 
         # calculate transformation with initial tranlation and lens function params but no rotation
-        xu, yu = self.transform(x, y, x0, y0, 0., A, B, C, D, unwarp=False)
+        xu, yu = self.transform(x, y, x0, y0, rl, 0., A, B, C, D, unwarp=False)
 
         # Find rotation matrix such that the vectors to the star locations roughly match
         Pu = np.array([xu, yu, np.zeros(len(x))]).T
@@ -119,44 +170,62 @@ class Calibrate:
         # Find euler angles of rotation matrix and select "z" rotation as an approximate theta
         theta = R.as_euler('xyz', degrees=True)[2]
 
-        return [x0, y0, theta, A, B, C, D]
+        return [x0, y0, rl, theta, C, D]
+
+    def save_calibration_params(self, configFile):
+
+        config = configparser.ConfigParser()
+        config.read(configFile)
+
+        config.set('CALIBRATION_PARAMS', 'X0', str(self.x0))
+        config.set('CALIBRATION_PARAMS', 'Y0', str(self.y0))
+        config.set('CALIBRATION_PARAMS', 'RL', str(self.rl))
+        config.set('CALIBRATION_PARAMS', 'THETA', str(self.theta))
+        config.set('CALIBRATION_PARAMS', 'A', str(self.A))
+        config.set('CALIBRATION_PARAMS', 'B', str(self.B))
+        config.set('CALIBRATION_PARAMS', 'C', str(self.C))
+        config.set('CALIBRATION_PARAMS', 'D', str(self.D))
+
+        with open(configFile, 'w') as cf:
+            config.write(cf)
 
 
-    def create_calibration_arrays(self):
-
-        # Create a grid and find the transformed coordinates of each grid point
-        i_grid, j_grid = np.meshgrid(np.arange(self.Imax),np.arange(self.Jmax))
-        x_grid, y_grid = self.normalize_pixel_coords(i_grid, j_grid)
-
-        self.xt_grid, self.yt_grid, lam_grid = self.transform(x_grid, y_grid, self.x0, self.y0, self.theta, self.A, self.B, self.C, self.D, return_elev=True)
-
-        # create mask
-        self.mask = np.zeros((self.Jmax, self.Imax), dtype=bool)
-        self.mask[lam_grid<self.elevCutoff*np.pi/180.] = True
-
-        # atmospheric corrections
-        self.atmosphericCorrection = self.atmospheric_corrections(lam_grid)
-        self.atmosphericCorrection[self.mask] = np.nan
-
-
-    def atmospheric_corrections(self, elev):
-        # Atmospheric corrections are taken from Kubota et al., 2001
-        # Kubota, M., Fukunishi, H. & Okano, S. Characteristics of medium- and
-        #   large-scale TIDs over Japan derived from OI 630-nm nightglow observation.
-        #   Earth Planet Sp 53, 741–751 (2001). https://doi.org/10.1186/BF03352402
-
-        # calculate zenith angle
-        za = np.pi/2-elev
-
-        # Kubota et al., 2001; eqn. 6
-        vanRhijnFactor = np.sqrt(1.0 - np.sin(za)**2*(self.RE/(self.RE+self.ha))**2)
-
-        # Kubota et al., 2001; eqn. 7,8
-        a = 0.2
-        F = 1. / (np.cos(za) + 0.15 * (93.885 - za*180./np.pi) ** (-1.253))
-        extinctionFactor = 10.0 ** (0.4 * a * F)
-
-        return vanRhijnFactor * extinctionFactor
+    # # move these functions to processing class
+    # def create_calibration_arrays(self):
+    #
+    #     # Create a grid and find the transformed coordinates of each grid point
+    #     i_grid, j_grid = np.meshgrid(np.arange(self.Imax),np.arange(self.Jmax))
+    #     x_grid, y_grid = self.normalize_pixel_coords(i_grid, j_grid)
+    #
+    #     self.xt_grid, self.yt_grid, lam_grid = self.transform(x_grid, y_grid, self.x0, self.y0, self.theta, self.A, self.B, self.C, self.D, return_elev=True)
+    #
+    #     # create mask
+    #     self.mask = np.zeros((self.Jmax, self.Imax), dtype=bool)
+    #     self.mask[lam_grid<self.elevCutoff*np.pi/180.] = True
+    #
+    #     # atmospheric corrections
+    #     self.atmosphericCorrection = self.atmospheric_corrections(lam_grid)
+    #     self.atmosphericCorrection[self.mask] = np.nan
+    #
+    #
+    # def atmospheric_corrections(self, elev):
+    #     # Atmospheric corrections are taken from Kubota et al., 2001
+    #     # Kubota, M., Fukunishi, H. & Okano, S. Characteristics of medium- and
+    #     #   large-scale TIDs over Japan derived from OI 630-nm nightglow observation.
+    #     #   Earth Planet Sp 53, 741–751 (2001). https://doi.org/10.1186/BF03352402
+    #
+    #     # calculate zenith angle
+    #     za = np.pi/2-elev
+    #
+    #     # Kubota et al., 2001; eqn. 6
+    #     vanRhijnFactor = np.sqrt(1.0 - np.sin(za)**2*(self.RE/(self.RE+self.ha))**2)
+    #
+    #     # Kubota et al., 2001; eqn. 7,8
+    #     a = 0.2
+    #     F = 1. / (np.cos(za) + 0.15 * (93.885 - za*180./np.pi) ** (-1.253))
+    #     extinctionFactor = 10.0 ** (0.4 * a * F)
+    #
+    #     return vanRhijnFactor * extinctionFactor
 
 
 
@@ -183,34 +252,35 @@ class Calibrate:
 
 
 
-    def save_calibration_file(self):
+    # def save_calibration_file(self):
+    #
+    #     with h5py.File(self.outputFile, 'w') as f:
+    #
+    #         newI = f.create_dataset('transformed_coords', data=np.array([self.xt_grid, self.yt_grid]), compression='gzip', compression_opts=1)
+    #         newI.attrs['Description'] = 'Transformed x, y coordinates of each point in the image grid'
+    #
+    #         backgroundCorrection = f.create_dataset('atmos_corr', data=self.atmosphericCorrection, compression='gzip', compression_opts=1)
+    #         backgroundCorrection.attrs['Description'] = 'Background atmospheric correction factors to apply to the image pre-regridding'
+    #
+    #         mask = f.create_dataset('mask', data=self.mask, compression='gzip', compression_opts=1)
+    #         backgroundCorrection.attrs['Description'] = 'Mask of portion of array that does not include the ASI FoV'
+    #
+    #         # transformation/calibration parameters
+    #         lensfun = f.create_dataset('lens_function', data=np.array([self.A, self.B, self.C, self.D]))
+    #         lensfun.attrs['Description'] = 'Lens function coefficients of the form [A,B,C,D] (A+Br+Cr^2+Dr^3)'
+    #
+    #         trans = f.create_dataset('translation', data=np.array([self.x0, self.y0]))
+    #         trans.attrs['Description'] = 'Linear translation [x0, y0]'
+    #
+    #         rotate = f.create_dataset('rotation_angle', data=self.theta)
+    #         rotate.attrs['Description'] = 'Rotation angle'
+    #
+    #         alt = f.create_dataset('altitude', data=self.ha)
+    #         alt.attrs['Description'] = 'Assumed airglow altituded used for atmospheric correction (km)'
+    #
+    #         elev = f.create_dataset('elevation_cutoff', data=self.elevCutoff)
+    #         elev.attrs['Description'] = 'Lowest allowed elevation angle'
 
-        with h5py.File(self.outputFile, 'w') as f:
-
-            newI = f.create_dataset('transformed_coords', data=np.array([self.xt_grid, self.yt_grid]), compression='gzip', compression_opts=1)
-            newI.attrs['Description'] = 'Transformed x, y coordinates of each point in the image grid'
-
-            backgroundCorrection = f.create_dataset('atmos_corr', data=self.atmosphericCorrection, compression='gzip', compression_opts=1)
-            backgroundCorrection.attrs['Description'] = 'Background atmospheric correction factors to apply to the image pre-regridding'
-
-            mask = f.create_dataset('mask', data=self.mask, compression='gzip', compression_opts=1)
-            backgroundCorrection.attrs['Description'] = 'Mask of portion of array that does not include the ASI FoV'
-
-            # transformation/calibration parameters
-            lensfun = f.create_dataset('lens_function', data=np.array([self.A, self.B, self.C, self.D]))
-            lensfun.attrs['Description'] = 'Lens function coefficients of the form [A,B,C,D] (A+Br+Cr^2+Dr^3)'
-
-            trans = f.create_dataset('translation', data=np.array([self.x0, self.y0]))
-            trans.attrs['Description'] = 'Linear translation [x0, y0]'
-
-            rotate = f.create_dataset('rotation_angle', data=self.theta)
-            rotate.attrs['Description'] = 'Rotation angle'
-
-            alt = f.create_dataset('altitude', data=self.ha)
-            alt.attrs['Description'] = 'Assumed airglow altituded used for atmospheric correction (km)'
-
-            elev = f.create_dataset('elevation_cutoff', data=self.elevCutoff)
-            elev.attrs['Description'] = 'Lowest allowed elevation angle'
     # def mock(self):
     #     print('WARNING: THIS IS A MOCK ROUTINE.  Instead of calculating new calibration parameters, it converts the old calibration files to the new format and assigns a pre-designated rotation angle.')
     #
@@ -260,27 +330,110 @@ class Calibrate:
     #     f.close()
 
 
+# def parse_args():
+#     """
+#     Handle the command line arguments.
+#     Returns:
+#     Output of argparse.ArgumentParser.parse_args.
+#     """
+#
+#     parser = argparse.ArgumentParser(description='Accepting config, input files and output file'
+#                                                  'to process MANGOImage.')
+#     parser.add_argument('-c', '--config', dest='config', type=str,
+#                         help='Config file containing data locations and image specs.')
+#     args = parser.parse_args()
+#
+#     return args
+#
+#
+# def main():
+#     command_line_args = parse_args()
+#     conf = command_line_args.config
+#     Calibrate(conf)
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+
 def parse_args():
-    """
-    Handle the command line arguments.
-    Returns:
-    Output of argparse.ArgumentParser.parse_args.
-    """
 
-    parser = argparse.ArgumentParser(description='Accepting config, input files and output file'
-                                                 'to process MANGOImage.')
-    parser.add_argument('-c', '--config', dest='config', type=str,
-                        help='Config file containing data locations and image specs.')
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Calculate camera calibration')
 
-    return args
+    parser.add_argument('station', help='Station code')
+    parser.add_argument('instrument', help='redline or greenline')
+
+    parser.add_argument('-c', '--config', metavar='FILE',
+                        help='Alternate configuration file')
+    parser.add_argument('-sc', '--starcal', metavar='FILE',
+                        help='Alternate starcal file')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Verbose output')
+
+    return parser.parse_args()
+
+
+def find_config(station, instrument):
+
+    # with h5py.File(filename) as h5:
+    #     station = h5['image'].attrs['station']
+    #     instrument = h5['image'].attrs['instrument']
+    path = os.path.dirname(__file__)
+    print(path)
+
+    config_file = '%s-%s.ini' % (station, instrument)
+
+    logging.debug('Using package configuration file: %s' % config_file)
+
+    full_path = os.path.join(path, 'data', config_file)
+
+    return full_path
+    # return resources.files('mangonetwork.raw.data').joinpath(config_file).read_text()
+
+def find_starcal(station, instrument):
+
+    starcal_file = 'starcal-%s-%s.txt' % (station, instrument)
+
+    logging.debug('Using package starcal file: %s' % starcal_file)
+
+    return resources.files('mangonetwork.raw.data').joinpath(starcal_file).read_text()
 
 
 def main():
-    command_line_args = parse_args()
-    conf = command_line_args.config
-    Calibrate(conf)
 
+    args = parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+
+    if args.config:
+        logging.debug('Alternate configuration file: %s' % args.config)
+        if not os.path.exists(args.config):
+            logging.error('Config file not found')
+            sys.exit(1)
+        config_path = args.config
+    else:
+        config_path = find_config(args.station, args.instrument)
+
+    # logging.debug('Configuration file: %s' % args.config)
+
+    if args.starcal:
+        logging.debug('Alternate starcal file: %s' % args.starcal)
+        if not os.path.exists(args.starcal):
+            logging.error('StarCal file not found')
+            sys.exit(1)
+        starcal_contents = open(args.starcal).read()
+    else:
+        starcal_contents = find_starcal(args.station, args.instrument)
+
+    # logging.debug('StarCal file: %s' % args.config)
+
+    Calibrate(config_path, starcal_contents)
+
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()

src/mangonetwork/raw/QuicklookMovies.py
@@ -31,7 +31,9 @@ class QuickLook:
         self.siteName = config.get('PROCESSING','SITE_NAME')
         self.siteState = config.get('PROCESSING','SITE_STATE')
         self.contrast = config.getint('PROCESSING','CONTRAST')
-        self.rotationAngle = config.getfloat('QUICKLOOK','ROTATION_ANGLE')
+        # self.rotationAngle = config.getfloat('QUICKLOOK','ROTATION_ANGLE')
+        self.rotationAngle = config.getfloat('CALIBRATION_PARAMS','THETA')
+        # self.rotationAngle = 0.
 
     def process_images(self, inputList, outputFile):
 
@@ -57,7 +59,9 @@ class QuickLook:
 
         cooked_image = MANGOImage(np.array(image))
         cooked_image.equalizeHistogram(self.contrast)
+        cooked_image.invertImage()
         cooked_image.rotateImage(self.rotationAngle)
+        # cooked_image.invertImage()
 
         start_time = datetime.datetime.utcfromtimestamp(image.attrs['start_time'])
         exposure_time = image.attrs['exposure_time']
@@ -67,7 +71,7 @@ class QuickLook:
         latlon = '%4.1f N, %5.1f W' % (image.attrs['latitude'],image.attrs['longitude'])
 
         fig, ax = plt.subplots()
-        ax.imshow(cooked_image.imageData, cmap='gray')
+        ax.imshow(cooked_image.imageData, cmap='gray', origin='upper')
 
         ax.annotate('N', xy=(350, 20), color='white')
         ax.annotate('E', xy=(620, 520//2), color='white')

src/mangonetwork/raw/StarCal.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import logging
+import io
 import sys
 if sys.version_info < (3,9):
     import importlib_resources as resources
@@ -44,6 +45,7 @@ class StarCal:
 
         cooked_image = MANGOImage(np.array(image))
         cooked_image.equalizeHistogram(contrast)
+        # cooked_image.invertImage()
         cooked_image.rotateImage(rotationAngle)
 
         # start_time = datetime.datetime.utcfromtimestamp(image.attrs['start_time'])
@@ -63,7 +65,11 @@ class StarCal:
         ax = fig.add_subplot(111)
         # ax.imshow(cooked_image.imageData, cmap='gray', origin='upper')
         ax.imshow(cooked_image.imageData, cmap='gray')
-        ax.scatter(695-i, j, facecolors='none', edgecolors='r')
+        ax.scatter(i, j, facecolors='none', edgecolors='r')
+
+        xp = 519./2.*np.cos(el)*np.sin(az)+695./2.
+        yp = 519./2.*np.cos(el)*np.cos(az)+519./2.
+        ax.scatter(xp, yp)
 
         # ax = fig.add_subplot(122)
         # xp = np.cos(el)*np.sin(az)
@@ -79,13 +85,15 @@ class StarCal:
 
         raw_filename = starCalFile.split('\n')[0].split()[-1]
 
-        data = list()
-        for line in starCalFile.split('\n'):
-            if line.startswith('#') or len(line)==0:
-                continue
-            data.append([float(x) for x in line.split()[1:5]])
+        az, el, i, j = np.loadtxt(io.StringIO(starCalFile), usecols=(1,2,3,4), unpack=True)
 
-        az, el, i, j = np.array(data).T
+        # data = list()
+        # for line in starCalFile.split('\n'):
+        #     if line.startswith('#') or len(line)==0:
+        #         continue
+        #     data.append([float(x) for x in line.split()[1:5]])
+        #
+        # az, el, i, j = np.array(data).T
 
         return az, el, i, j, raw_filename
 
@@ -94,25 +102,27 @@ def parse_args():
 
     parser = argparse.ArgumentParser(description='Check the star identification for calibration')
 
-    parser.add_argument('imager', help='Imager code (ie. CFSred or BLOgreen)')
-    parser.add_argument('-c', '--config', metavar='FILE',
+    parser.add_argument('station', help='Station code')
+    parser.add_argument('instrument', help='redline or greenline')
+
+    parser.add_argument('-sc', '--starcal', metavar='FILE',
                         help='Alternate starcal file')
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='Verbose output')
 
     return parser.parse_args()
 
-def find_config(imager):
+def find_starcal(station, instrument):
 
     # with h5py.File(filename) as h5:
     #     station = h5['image'].attrs['station']
     #     instrument = h5['image'].attrs['instrument']
 
-    name = 'starcal-%s.txt' % (imager)
+    starcal_file = 'starcal-%s-%s.txt' % (station, instrument)
 
-    logging.debug('Using package starcal file: %s' % name)
+    logging.debug('Using package starcal file: %s' % starcal_file)
 
-    return resources.files('mangonetwork.raw.data').joinpath(name).read_text()
+    return resources.files('mangonetwork.raw.data').joinpath(starcal_file).read_text()
 
 def main():
 
@@ -124,16 +134,16 @@ def main():
         logging.basicConfig(level=logging.INFO)
 
 
-    if args.config:
-        logging.debug('Alternate starcal file: %s' % args.config)
-        if not os.path.exists(args.config):
+    if args.starcal:
+        logging.debug('Alternate starcal file: %s' % args.starcal)
+        if not os.path.exists(args.starcal):
             logging.error('Config file not found')
             sys.exit(1)
-        contents = open(args.config).read()
+        contents = open(args.starcal).read()
     else:
-        contents = find_config(args.imager)
+        contents = find_starcal(args.station, args.instrument)
 
-    logging.debug('Configuration file: %s' % args.config)
+    # logging.debug('Configuration file: %s' % args.starcal)
 
     StarCal(contents)
 